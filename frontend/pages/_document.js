71464df808248e56b329d6cde318b9a9dca1dccc
vFlav <flv691@mail.usask.ca>
2018-10-18 19:14:33 -0600

    Add _document JS for collecting styles from the react component tree

---
+++ frontend/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+// collects all the styles in the React component tree for rendering
+export default class MyDocument extends Document {
+    static getInitialProps({ renderPage }) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props => sheet.collectStyles(<App { ...props }/>));
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
+    }
+
+    render() {
+        return (
+            <html>
+                <Head>{this.props.styleTags}</Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </html>
+        )
+    }
+}
