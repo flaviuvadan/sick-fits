d8bea99565e3ef9f5b1cf4a721e52f960ee53752
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-11 16:42:17 -0600

    Add dogs example query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,17 @@
-const Query = {};
+const Query = {
+    /**
+     * Dogs query
+     * @param parent
+     * @param args - arguments passed to dogs
+     * @param ctx - context of the request
+     * @param info - additional info
+     */
+    dogs(parent, args, ctx, info) {
+        return [
+            {name: 'Snickers'},
+            {'name': 'Sunny'},
+            ]
+    }
+};
 
 module.exports = Query;
