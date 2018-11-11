acd1c3a4965d6022610c099e149a343a714e3073
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-11 17:31:11 -0600

    Re-write items by using forwardTo and add MD for adding a new type

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,14 +1,18 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-    /**
-     * Get items
-     * @param parent -
-     * @param args - arguments of items
-     * @param ctx - context of request
-     * @param info - additional info
-     */
-    async items(parent, args, ctx, info) {
-        return await ctx.db.query.items();
-    }
+    // forwardto('db') for items is the same as implementing as the items() resolver
+    items: forwardTo('db'),
+    // /**
+    //  * Get items
+    //  * @param parent -
+    //  * @param args - arguments of items
+    //  * @param ctx - context of request
+    //  * @param info - additional info
+    //  */
+    // async items(parent, args, ctx, info) {
+    //     return await ctx.db.query.items();
+    // }
 };
 
 module.exports = Query;
