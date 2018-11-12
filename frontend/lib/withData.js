34923928826cdc946e27507b8802015405b9f427
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-11 18:46:30 -0600

    Implement getInitialProps in app

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,10 +1,17 @@
+// a high order component that exposes the Apollo client via a prop
+// want server side rendering and next-with-apollo helps with that
 import withApollo from 'next-with-apollo';
+
+// package by Apollo containing all the std things one may use
+// can extend what the Apollo client does and contains a lot of pre-configed things
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
 
+// header important for authentication
 function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+      // include credentials on every request, includes cookies
     request: operation => {
       operation.setContext({
         fetchOptions: {
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,36 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
 
-class MyApp extends App {
-    render() {
-        const { Component } = this.props;
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
-        return (
-            <Container>
-                {/*This paragraph is on every page and Next.js renders whatever component is loaded as part of the*/}
-                {/*<Component/>, the reason it is imported. It is the ReactChild of the parent component*/}
-                <Page>
-                    <Component/>
-                </Page>
-            </Container>
-        )
-    }
+class MyApp extends App {
+	static async getInitialProps({ Component, ctx }) {
+		let pageProps = {};
+		// crawl pages and fetch data
+		if (Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+		}
+		// this exposes the query to the user
+		pageProps.query = ctx.query;
+		return { pageProps };
+	}
+
+	render() {
+		const { Component, apollo, pageProps } = this.props;
+
+		return (
+			<Container>
+				<ApolloProvider client={this.props.apollo}>
+					{/*This paragraph is on every page and Next.js renders whatever component is loaded as part of the*/}
+					{/*<Component/>, the reason it is imported. It is the ReactChild of the parent component*/}
+					<Page>
+						<Component { ...pageProps }/>
+					</Page>
+				</ApolloProvider>
+			</Container>
+		)
+	}
 }
 
-export default MyApp;
+export default withData(MyApp);
