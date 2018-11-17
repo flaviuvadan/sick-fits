c1fc4a829d925cf9c1bbac57e541fcac9ad98b4c
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-16 19:43:52 -0600

    Add uploadFile method and add HTML definition

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -29,6 +29,21 @@ class CreateItem extends Component {
 		this.setState({ [name]: val });
 	};
 
+	/**
+	 * Responsible for uploading images to Cloudinary
+	 * @param e - DOM event
+	 */
+	uploadFile =  async e => {
+		const files = e.target.files;
+		const data = new FormData();
+		data.append('file', files[0]);
+		data.append('upload_preset', 'sick-fits');
+		const payload = { method: 'POST', body: data };
+		const res = await fetch('https://api.cloudinary.com/v1_1/vflav/image/upload', payload);
+		const file = await res.json();
+		this.setState({ image: file.secure_url, largeImage: file.eager[0].secure_url });
+	};
+
 	render() {
 		return (
 			<Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -46,6 +61,16 @@ class CreateItem extends Component {
 					}}>
 						<Error error={error}/>
 						<fieldset disabled={loading} aria-busy={loading}>
+							<label htmlFor="file">
+								Image
+								<input type="file"
+									   id="file"
+									   name="file"
+									   placeholder="Upload an image"
+									   required
+									   onChange={this.uploadFile}
+								/>
+							</label>
 							<label htmlFor="title">
 								Title
 								<input type="text"
