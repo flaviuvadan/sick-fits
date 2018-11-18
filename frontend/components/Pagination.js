580359204a5b2b4deff329589ff7d148e6cdc216
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-18 16:53:43 -0600

    Refactor Pagination

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
-import PaginationStyles from './styles/PaginationStyles';
+import Link from 'next/link';
+import Head from 'next/head';
 
+import PaginationStyles from './styles/PaginationStyles';
 import { Query } from 'react-apollo';
 import { PAGINATION_QUERY } from '../queries/queries';
 import { perPage } from '../config';
 
 export default class Pagination extends Component {
 	render() {
-		return (
-			<PaginationStyles>
-				<Query query={PAGINATION_QUERY}>
-					{({ data, loading, error }) => {
-						if (loading) return <p>Loading...</p>;
-						const count = data.itemsConnection.aggregate.count;
-						const pages = Math.ceil(count / perPage);
-						return <p>Page {this.props.page} of {pages}</p>
-					}}
-				</Query>
-			</PaginationStyles>
+		return (<Query query={PAGINATION_QUERY}>
+				{({ data, loading, error }) => {
+					if (loading) return <p>Loading...</p>;
+					const count = data.itemsConnection.aggregate.count;
+					const pages = Math.ceil(count / perPage);
+					const page = this.props.page;
+					return (
+						<PaginationStyles>
+							<Head>
+								<title>Sick Fits | Page {page} of {pages}</title>
+							</Head>
+							<Link prefetch href={{
+								pathname: 'items',
+								query: { page: page - 1 }
+							}}>
+								<a>Prev</a>
+							</Link>
+							<p>Page {page} of {pages}</p>
+						</PaginationStyles>
+					)
+				}}
+			</Query>
 		)
 	}
 }
