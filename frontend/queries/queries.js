9e66e40c242de521509de21e20d45fc8812c3e12
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-18 17:40:56 -0600

    Adjust ALL_ITEMS_QUERY to take in pagination params

--- frontend/queries/queries.js
+++ frontend/queries/queries.js
@@ -1,8 +1,17 @@
 import gql from 'graphql-tag';
 
+import { perPage } from '../config';
+
 export const ALL_ITEMS_QUERY = gql`
-    query ALL_ITEMS_QUERY {
-        items {
+    query ALL_ITEMS_QUERY(
+		$skip: Int = 0
+		$first: Int = ${perPage}
+	) {
+        items(
+			first: $first 
+			skip: $skip
+			orderBy: createdAt_DESC
+		) {
             id
             title
             price
