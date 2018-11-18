0d8e68bcf98522391ec5e3d8636c41babfc993c3
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-18 16:29:52 -0600

    Get aggregate count from PAGINATION_QUERY

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import PaginationStyles from './styles/PaginationStyles';
 
+import { Query } from 'react-apollo';
+import { PAGINATION_QUERY } from '../queries/queries';
+import { perPage } from '../config';
+
 export default class Pagination extends Component {
 	render() {
 		return (
 			<PaginationStyles>
-				<p>Hi, pagination!</p>
+				<Query query={PAGINATION_QUERY}>
+					{({ data, loading, error }) => {
+						if (loading) return <p>Loading...</p>;
+						const count = data.itemsConnection.aggregate.count;
+						const pages = Math.ceil(count / perPage);
+						return <p>Page 1 of {pages}</p>
+					}}
+				</Query>
 			</PaginationStyles>
 		)
 	}
