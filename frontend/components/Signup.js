617d4f62557dd5b7a3a48ace91556ecbe9a9d5e7
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-19 20:03:04 -0600

    Start adding signup forms

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -5,11 +5,45 @@ import Error from './ErrorMessage';
 
 
 class Signup extends Component {
+	state = {
+		email: '',
+		name: '',
+		password: ''
+	};
+
+	saveToState = () => {
+		console.log('saveToState');
+	};
+
 	render() {
 		return (
 			<Form>
 				<fieldset>
-					<h2>Sign up</h2>
+					<h2>Sign Up for An Account</h2>
+					<label htmlFor="email">
+						Email
+						<input type="text"
+							   name="email"
+							   placeholder="email"
+							   value={this.state.email}
+							   onChange={this.saveToState}/>
+					</label>
+					<label htmlFor="name">
+						Name
+						<input type="text"
+							   name="name"
+							   placeholder="name"
+							   value={this.state.name}
+							   onChange={this.saveToState}/>
+					</label>
+					<label htmlFor="password">
+						Password
+						<input type="text"
+							   name="password"
+							   placeholder="password"
+							   value={this.state.password}
+							   onChange={this.saveToState}/>
+					</label>
 				</fieldset>
 			</Form>
 		)
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,10 +1,18 @@
-import React from 'react';
 import Signup from '../components/Signup';
+import styled from 'styled-components';
+
+const Columns = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+	grid-gap: 20px;
+`;
 
 const SignupPage = props => (
-	<div>
+	<Columns>
 		<Signup/>
-	</div>
+		<Signup/>
+		<Signup/>
+	</Columns>
 );
 
 export default SignupPage;
