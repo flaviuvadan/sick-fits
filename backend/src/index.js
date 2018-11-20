cc11e88f248e31bf03f96d512f386537da345907
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-19 19:03:37 -0600

    Add cookie middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,30 @@
 // start up the Node server
-require('dotenv').config({
-    path: 'variables.env'
-});
-
+const cookieParser = require('cookie-parser');
 const createServer = require('./createServer');
 const db = require('./db');
 
+require('dotenv').config({
+	path: 'variables.env'
+});
+
 const server = createServer();
 
+// middleware - some function that will run in the middle, between a request and a response
+// involved in processes such as authentication, data transformation, etc.
 // TODO: use express middleware to handle cookie (JWT)
+// allow access to cookies in a nice format
+server.express.use(cookieParser());
+
 // TODO: use express middleware to populate current users
 
 server.start(
-    {
-        cors: {
-            credentials: true,
-            origin: process.env.FRONTEND_URL,
-        },
-    },
-    deets => {
-        console.log(`Server is now running on port ${deets.port}`);
-    },
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL,
+		},
+	},
+	deets => {
+		console.log(`Server is now running on port ${deets.port}`);
+	},
 );
