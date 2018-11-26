63ad029200cbf9d086e15472d89d9f4d9c4766af
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-25 19:40:16 -0600

    Add sign in mutation and implement sign in component

---
+++ frontend/components/Signin.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import Form from './styles/Form';
+import Error from './ErrorMessage';
+import { SIGNIN_MUTATION } from "../queries/queries";
+
+class Signin extends Component {
+	state = {
+		email: '',
+		password: ''
+	};
+
+	/**
+	 * Saves values to state
+	 * @param e - event
+	 */
+	saveToState = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	render() {
+		return (
+			<Mutation mutation={SIGNIN_MUTATION} variables={this.state}>
+				{(signin, { error, loading }) => {
+					return (
+
+						<Form method="post" onSubmit={async e => {
+							e.preventDefault();
+							await signin();
+							this.setState({ email: '', password: ''});
+						}}>
+							<fieldset disabled={loading} aria-busy={loading}>
+								<h2>Sign Into Your Account</h2>
+								<Error error={error}/>
+								<label htmlFor="email">
+									Email
+									<input type="text"
+										   name="email"
+										   placeholder="email"
+										   value={this.state.email}
+										   onChange={this.saveToState}/>
+								</label>
+
+								<label htmlFor="password">
+									Password
+									<input type="password"
+										   name="password"
+										   placeholder="password"
+										   value={this.state.password}
+										   onChange={this.saveToState}/>
+								</label>
+
+								<button type="submit">Sign In!</button>
+							</fieldset>
+						</Form>
+					)
+				}}
+			</Mutation>
+		)
+	}
+}
+
+export default Signin;
--- frontend/queries/queries.js
+++ frontend/queries/queries.js
@@ -118,3 +118,19 @@ export const SIGNUP_MUTATION = gql`
         }
     }
 `;
+
+export const SIGNIN_MUTATION = gql`
+    mutation SIGNIN_MUTATION(
+    $email: String!
+    $password: String!
+    ) {
+        signin(
+            email: $email
+            password: $password
+        ) {
+            id
+            email
+            name
+        }
+    }
+`;
