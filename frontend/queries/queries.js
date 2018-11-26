f8882efeaed75a6c9600a4fa8996caf7fc7d8325
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-25 18:31:39 -0600

    Implement signup mutation

--- frontend/queries/queries.js
+++ frontend/queries/queries.js
@@ -100,3 +100,21 @@ export const PAGINATION_QUERY = gql`
     	}
     }
 `;
+
+export const SIGNUP_MUTATION = gql`
+    mutation SIGNUP_MUTATION(
+        $email: String!
+        $name: String!
+        $password: String!
+    ) {
+        signup(
+        	email: $email
+        	name: $name
+            password: $password
+        ) {
+            id
+            email
+            name
+        }
+    }
+`;
