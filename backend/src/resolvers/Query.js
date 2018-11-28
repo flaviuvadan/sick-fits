99b94d4af34ca48346a0df2a25c4cf92be758579
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-27 19:11:25 -0600

    Add currentUser query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,20 +1,41 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-    // forwardto('db') for items is the same as implementing as the items() resolver
-    items: forwardTo('db'),
+	// forwardto('db') for items is the same as implementing as the items() resolver
+	items: forwardTo('db'),
 	item: forwardTo('db'),
 	itemsConnection: forwardTo('db'),
-    // /**
-    //  * Get items
-    //  * @param parent -
-    //  * @param args - arguments of items
-    //  * @param ctx - context of request
-    //  * @param info - additional info
-    //  */
-    // async items(parent, args, ctx, info) {
-    //     return await ctx.db.query.items();
-    // }
+	// /**
+	//  * Get items
+	//  * @param parent -
+	//  * @param args - arguments of items
+	//  * @param ctx - context of request
+	//  * @param info - additional info
+	//  */
+	// async items(parent, args, ctx, info) {
+	//     return await ctx.db.query.items();
+	// }
+
+	/**
+	 * @param parent
+	 * @param args - arguments of signup
+	 * @param ctx - context of request
+	 * @param info - additional info, actual query coming from client side
+	 * @returns {Promise<void>}
+	 */
+	currentUser(parent, args, ctx, info) {
+		// check if there is a current userId
+		if (!ctx.request.userId) {
+			// important to return null in this case
+			return null;
+		} else {
+			return ctx.db.query.user({
+				where: {
+					id: ctx.request.userId
+				}
+			}, info);
+		}
+	},
 };
 
 module.exports = Query;
