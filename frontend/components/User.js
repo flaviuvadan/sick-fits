52f61ee65d33e23e8e0fb4c472042202a6b68a18
FlaviuVadan <flaviuvadan@gmail.com>
2018-11-27 19:18:50 -0600

    Add User prop component

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,3 +1,16 @@
 import { Query } from 'react-apollo';
 import PropTypes from 'prop-types';
 
+import { CURRENT_USER_QUERY } from "../queries/queries";
+
+const User = props => (
+	<Query {...props} query={CURRENT_USER_QUERY}>
+		{payload => props.children(payload)}
+	</Query>
+);
+
+User.PropTypes = {
+	children: PropTypes.func.isRequired,
+};
+
+export default User;
