d1d27d8c0135cd97b2f76ca5f46a1e4c2a45453f
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-02 17:14:00 -0600

    Add GraphQL UPDATE_PERMISSIONS_MUTATION

--- frontend/queries/queries.js
+++ frontend/queries/queries.js
@@ -194,3 +194,20 @@ export const ALL_USERS_QUERY = gql`
 		}
 	}
 `;
+
+export const UPDATE_PERMISSIONS_MUTATION = gql`
+	mutation UPDATE_PERMISSIONS_MUTATION (
+		$permissions: [Permission]
+		$userId: ID!
+	) {
+		updatePermissions(
+			permissions: $permissions
+			userId: $userId
+		) {
+			id
+			name
+			email
+			permissions
+		}
+	}
+`;
