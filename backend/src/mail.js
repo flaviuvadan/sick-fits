b2aa8d466269169ba6069247e9952675229a009a
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-01 18:03:02 -0600

    Add makeEmail function and transport for SMTP

---
+++ backend/src/mail.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+	host: process.env.MAIL_HOST,
+	post: process.env.MAIL_PORT,
+	auth: {
+		user: process.env.MAIL_USER,
+		pass: process.env.MAIL_PASS,
+	}
+});
+
+
+const makeEmail = text => `
+	<div className="email" style="
+		border: 1px solid black;
+		padding: 20px;
+		font-family: sans-serif;
+		line-height: 2;
+		font-size: 20px;">
+		<h2>Hello,</h2>
+		<br>
+		<p>${text}</p>
+		<br>
+		<p>Sick Fits</p>
+	</div>
+`;
+
+exports.transport = transport;
+exports.makeEmail = makeEmail;
