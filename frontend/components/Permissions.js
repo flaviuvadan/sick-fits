6080a38aee87138e1d2299c0eb3000c194a3f8d5
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-02 08:18:30 -0600

    Store local changes to permissions

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
+import PropTypes from 'prop-types';
 import Error from './ErrorMessage';
 import { ALL_USERS_QUERY } from "../queries/queries";
 import Table from './styles/Table';
@@ -30,7 +31,7 @@ const Permissions = props => (
 							<th>Action</th>
 						</tr>
 						</thead>
-						<tbody>{data.users.map(user => <User user={user} key={user.id}/>)}</tbody>
+						<tbody>{data.users.map(user => <UserPermissions user={user} key={user.id}/>)}</tbody>
 					</Table>
 				</div>
 			</div>
@@ -38,7 +39,40 @@ const Permissions = props => (
 	</Query>
 );
 
-class User extends Component {
+class UserPermissions extends Component {
+	static propTypes = {
+		user: PropTypes.shape({
+			name: PropTypes.string,
+			email: PropTypes.string,
+			id: PropTypes.string,
+			permissions: PropTypes.array,
+		}).isRequired,
+	};
+
+	// part of component, can be updated via this.setState()
+	state = {
+		// this is considered bad practice because props will not change the value of permissions
+		// however, we are seeding the data here , update it later, and then send it with a mutation
+		permissions: this.props.user.permissions,
+	};
+
+	/**
+	 * Handle permission changes on the table
+	 * @param e - event
+	 */
+	handlePermissionChange = e => {
+		const checkbox = e.target;
+		// copy current permissions (deep copy)
+		let updatedPermissions = [...this.state.permissions];
+
+		if (checkbox.checked) {
+			updatedPermissions.push(checkbox.value);
+		} else {
+			updatedPermissions = updatedPermissions.filter(permission => permission !== checkbox.value);
+		}
+		this.setState({ permissions: updatedPermissions });
+	};
+
 	render() {
 		const user = this.props.user;
 		return (
@@ -49,7 +83,10 @@ class User extends Component {
 					return (
 						<td key={`${user.id}-permission-${permission}`}>
 							<label htmlFor={`${user.id}-permission-${permission}`}>
-								<input type="checkbox"></input>
+								<input type="checkbox"
+									   checked={this.state.permissions.includes(permission)}
+									   value={permission}
+									   onChange={this.handlePermissionChange}/>
 							</label>
 						</td>
 					)
