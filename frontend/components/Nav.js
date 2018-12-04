5d5ea2f976a8d08cef460c1ecc20ceccddfe16ef
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-03 19:06:09 -0600

    Add cart button

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import NavStyles from './styles/NavStyles';
-
+import { Mutation } from 'react-apollo';
 import User from './User';
 import Signout from './Signout';
+import { TOGGLE_CART_MUTATION } from "../queries/queries";
 
 
 // note, a stateless functional component
@@ -26,6 +27,10 @@ const Nav = () => (
 							<a>Account</a>
 						</Link>
 						<Signout/>
+						<Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
+							<button onClick={toggleCart}>My Cart</button>
+						)}
+						</Mutation>
 					</>
 				)}
 
