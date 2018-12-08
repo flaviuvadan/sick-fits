60f1118573a68cc68612dfd908f2b28230c5891a
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-08 17:27:57 -0600

    Refactor Cart to avoid nesting too many queries - implement Composed

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Query, Mutation } from 'react-apollo'
+import { adopt } from 'react-adopt';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
@@ -11,44 +12,41 @@ import formatMoney from '../lib/formatMoney';
 import { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION } from "../queries/queries";
 
 
+const Composed = adopt({
+	user: ({ render }) => <User>{render}</User>,
+	toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
+	localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
+});
+
 const Cart = () => (
-	<User>
-		{({ data: { currentUser } }) => {
+	<Composed>
+		{({ user, toggleCart, localState }) => {
+			const currentUser = user.data.currentUser;
 			if (!currentUser) return null;
+
 			return (
-				<Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
-					<Query query={LOCAL_STATE_QUERY}>
-						{({ data }) => {
-							return (
-								<CartStyles open={data.cartOpen}>
-									<header>
-										<CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
-										<Supreme>Cart</Supreme>
-										<p>
-											You have {currentUser.cart.length} item{currentUser.cart.length === 1 ? '' :
-											's'} in your cart
-										</p>
-									</header>
-									<ul>
-										{currentUser.cart.map(cartItem => {
-											return (
-												<CartItem key={cartItem.id} cartItem={cartItem}/>
-											)
-										})}
-									</ul>
-									<footer>
-										<p>{formatMoney(calcTotalPrice(currentUser.cart))}</p>
-										<SickButton>Checkout</SickButton>
-									</footer>
-								</CartStyles>
-							);
-						}}
-					</Query>
-				)}
-				</Mutation>
-			)
+				<CartStyles open={localState.data.cartOpen}>
+					<header>
+						<CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
+						<Supreme>Cart</Supreme>
+						<p>
+							You have {currentUser.cart.length} item{currentUser.cart.length === 1 ? '' :
+							's'} in your cart
+						</p>
+					</header>
+					<ul>
+						{currentUser.cart.map(cartItem => {
+							return <CartItem key={cartItem.id} cartItem={cartItem}/>
+						})}
+					</ul>
+					<footer>
+						<p>{formatMoney(calcTotalPrice(currentUser.cart))}</p>
+						<SickButton>Checkout</SickButton>
+					</footer>
+				</CartStyles>
+			);
 		}}
-	</User>
+	</Composed>
 );
 
 export default Cart;
