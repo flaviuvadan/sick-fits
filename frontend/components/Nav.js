19310b8904abd68151bf77992fd28442d5ac52de
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-08 16:32:38 -0600

    Reduce number of cartItems to get total quantity

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -31,7 +31,9 @@ const Nav = () => (
 						<Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
 							<button onClick={toggleCart}>
 								My Cart
-								<CartCount count={100}/>
+								<CartCount count={currentUser.cart.reduce((total, cartItem) => {
+									return total + cartItem.quantity;
+								}, 0)}/>
 							</button>
 						)}
 						</Mutation>
