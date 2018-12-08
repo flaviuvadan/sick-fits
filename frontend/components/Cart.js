531bcffc51360e14c358b6e5582845511589ecd3
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-08 10:43:17 -0600

    Use the User component in Cart (access info about currentUser)

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -4,32 +4,36 @@ import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
+import User from '../components/User';
 import { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION } from "../queries/queries";
 
 const Cart = () => (
-	<Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
+	<User>{({ data: { currentUser } }) => {
+		if (!currentUser) return null;
+		return (
+			<Mutation mutation={TOGGLE_CART_MUTATION}>{(toggleCart) => (
+				<Query query={LOCAL_STATE_QUERY}>
+					{({ data }) => {
+						return (
+							<CartStyles open={data.cartOpen}>
+								<header>
+									<CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
+									<Supreme>Cart</Supreme>
+									<p>You have ... items in your cart</p>
+								</header>
 
-		<Query query={LOCAL_STATE_QUERY}>
-			{({ data }) => {
-
-				return (
-					<CartStyles open={data.cartOpen}>
-						<header>
-							<CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
-							<Supreme>Your Cart</Supreme>
-							<p>You have ... items in your cart</p>
-						</header>
-
-						<footer>
-							<p>$10.00</p>
-							<SickButton>Checkout</SickButton>
-						</footer>
-					</CartStyles>
-				);
-			}}
-		</Query>
-	)}
-	</Mutation>
+								<footer>
+									<p>$10.00</p>
+									<SickButton>Checkout</SickButton>
+								</footer>
+							</CartStyles>
+						);
+					}}
+				</Query>
+			)}
+			</Mutation>
+		)
+	}}</User>
 );
 
 export default Cart;
