862710da0ed62ff786f933ad4c1d4784686e2eac
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-08 10:55:07 -0600

    Pass cartItem via prop and add propType required to CartItem component

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import formatMoney from '../lib/formatMoney';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
 
 const CartItemStyles = styled.li`
 
@@ -9,9 +11,13 @@ const CartItemStyles = styled.li`
 const CartItem = props => {
 	return (
 		<CartItemStyles>
-			<p>HI</p>
+			{props.cartItem.id}
 		</CartItemStyles>
 	)
+};
+
+CartItem.propTypes = {
+	cartItem: PropTypes.object.isRequired,
 };
 
 export default CartItem;
