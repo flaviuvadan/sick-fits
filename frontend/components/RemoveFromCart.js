b859f59f0cf7fa6ea164561ef0d876bbcfd2a1b3
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-08 11:32:40 -0600

    Add RemoveFromCart component

---
+++ frontend/components/RemoveFromCart.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Mutation } from 'react-apollo';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { CURRENT_USER_QUERY } from "../queries/queries";
+
+
+const RemoveButton = styled.button`
+	font-size: 3rem;
+	background: none;
+	border: 0;
+	
+	&:hover {
+		color: ${props => props.theme.red};
+		cursor: pointer;
+	}
+`;
+
+class RemoveFromCart extends React.Component {
+	render() {
+		return (
+			<RemoveButton title="Remove Item">&times;</RemoveButton>
+		);
+	}
+}
+
+export default RemoveFromCart;
