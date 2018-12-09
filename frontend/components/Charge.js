8d67ab30535841c88162d24553fe8174d55f55a7
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-09 10:58:44 -0600

    Add basic Charge component

---
+++ frontend/components/Charge.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { Mutation } from 'react-apollo';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import PropTypes from 'prop-types';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import Error from './ErrorMessage';
+import User from './User';
+import { CURRENT_USER_QUERY } from "../queries/queries";
+
+
+class Charge extends React.Component {
+	render() {
+		return (
+			<User>
+				{({ data: { currentUser } }) => <p>Checkout</p>}
+			</User>
+		)
+	}
+}
+
+export default Charge;
