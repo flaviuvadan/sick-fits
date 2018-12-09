a37f2db8bc65c3719a5210efc5b78ae21b21f6f2
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-09 11:07:46 -0600

    Implement StripeCheckout

--- frontend/components/Charge.js
+++ frontend/components/Charge.js
@@ -10,11 +10,32 @@ import User from './User';
 import { CURRENT_USER_QUERY } from "../queries/queries";
 
 
+/**
+ * Get total number of items from users' cart
+ * @param cart - currentUser.cart
+ * @returns number of items
+ */
+function totalItems(cart) {
+	return cart.reduce((total, cartItem) => total + cartItem.quantity, 0);
+}
+
 class Charge extends React.Component {
 	render() {
 		return (
 			<User>
-				{({ data: { currentUser } }) => <p>{this.props.children}</p>}
+				{({ data: { currentUser } }) => (
+					<StripeCheckout
+						amount={calcTotalPrice(currentUser.cart)}
+						name="Sick Fits"
+						description={`Order of ${totalItems(currentUser.cart)} items`}
+						image={currentUser.cart[0].item && currentUser.cart[0].item.image}
+						stripeKey="pk_test_kMeWdXYUASgLL9oF8dI502Pu"
+						currency="USD"
+						email={currentUser.email}
+					>
+						{this.props.children}
+					</StripeCheckout>
+				)}
 			</User>
 		)
 	}
