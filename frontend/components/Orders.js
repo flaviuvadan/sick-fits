41d788709404fdcd0833e399b6c20f390241829b
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-14 19:46:30 -0600

    Add preliminary import statements

--- frontend/components/Orders.js
+++ frontend/components/Orders.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import { formatDistance } from 'date-fns';
+import Link from 'next/link';
+import styled from 'styled-components';
+import formatMoney from '../lib/formatMoney';
+import OrderItemStyles from './styles/OrderItemStyles';
+
 
 class Orders extends Component {
 	render() {
 		return (
 			<div>
-				
+				<p>Order list</p>
 			</div>
 		);
 	}
