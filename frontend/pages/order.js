54733f04d667eea8efbb9768b80ec3c422a40bb9
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-14 19:08:00 -0600

    Add basic order page

---
+++ frontend/pages/order.js
@@ -0,0 +1,12 @@
+import SigninWarning from '../components/SignInWarning';
+
+
+const Order = () => (
+	<div>
+		<SigninWarning>
+			<p>This is a single order</p>
+		</SigninWarning>
+	</div>
+);
+
+export default Order;
