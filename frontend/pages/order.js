2f2c5d9dcda8276a8eb10a92f99b12fb8cbea99a
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-14 19:12:02 -0600

    Add skeleton Order component

--- frontend/pages/order.js
+++ frontend/pages/order.js
@@ -1,10 +1,11 @@
 import SigninWarning from '../components/SignInWarning';
+import Order from '../components/Order';
 
-
-const Order = () => (
+const Order = props => (
 	<div>
 		<SigninWarning>
 			<p>This is a single order</p>
+			<p>This is the id {props.query.id}</p>
 		</SigninWarning>
 	</div>
 );
