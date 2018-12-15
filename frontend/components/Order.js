fb7ee13fe5381b4f82657332dfe5bbf35a05c41a
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-14 19:15:38 -0600

    Add future require imports

--- frontend/components/Order.js
+++ frontend/components/Order.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import { format } from 'date-fns';
+// allows to change title
+import Head from 'next/head';
+import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
+import OrderStyles from './styles/OrderItemStyles';
 
 
 class Order extends Component {
