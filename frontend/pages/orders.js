33f3dc0587a65996d441adfd9dec7635e453b0f7
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-14 19:42:38 -0600

    Add skeleton order page

---
+++ frontend/pages/orders.js
@@ -0,0 +1,13 @@
+import SigninWarning from '../components/SignInWarning';
+
+
+
+const OrdersPage = props => (
+	<div>
+		<SigninWarning>
+			<p>Hello</p>
+		</SigninWarning>
+	</div>
+);
+
+export default OrdersPage;
