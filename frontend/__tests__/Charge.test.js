0b3d5409778de738cc08e6a8644813ca9eedc7b0
FlaviuVadan <flaviuvadan@gmail.com>
2018-12-17 19:16:01 -0600

    Add Charge tests

--- frontend/__tests__/Charge.test.js
+++ frontend/__tests__/Charge.test.js
@@ -4,9 +4,8 @@ import NProgress from 'nprogress';
 import wait from 'waait';
 import toJSON from 'enzyme-to-json';
 import { MockedProvider } from 'react-apollo/test-utils';
-import { ApolloConsumer } from 'react-apollo';
 import Charge from '../components/Charge';
-import { CREATE_ORDER_MUTATION, CURRENT_USER_QUERY } from "../queries/queries";
+import { CURRENT_USER_QUERY } from "../queries/queries";
 import { fakeUser, fakeCartItem } from "../lib/testUtils";
 
 
@@ -42,5 +41,74 @@ describe('<Charge/>', () => {
 		wrapper.update();
 		const checkoutButton = wrapper.find('ReactStripeCheckout');
 		expect(toJSON(checkoutButton)).toMatchSnapshot();
+	});
+
+	it('creates an order ontoken', async () => {
+		const createOrderMock = jest.fn().mockResolvedValue({
+			data: {
+				createOrder: {
+					id: 'abc123',
+				},
+			},
+		});
+		const wrapper = mount(
+			<MockedProvider mocks={mocks}>
+				<Charge/>
+			</MockedProvider>
+		);
+		const component = wrapper.find('Charge').instance();
+		//manually call onToken
+		component.onToken({ id: 'abc123' }, createOrderMock);
+		expect(createOrderMock).toHaveBeenCalled();
+		expect(createOrderMock).toHaveBeenCalledWith({ "variables": { "token": "abc123" } });
+	});
+
+	it('turns progress bar on', async () => {
+		const wrapper = mount(
+			<MockedProvider mocks={mocks}>
+				<Charge/>
+			</MockedProvider>
+		);
+		await wait();
+		wrapper.update();
+		NProgress.start = jest.fn();
+		const createOrderMock = jest.fn().mockResolvedValue({
+			data: {
+				createOrder: {
+					id: 'abc123',
+				},
+			},
+		});
+		const component = wrapper.find('Charge').instance();
+		//manually call onToken
+		component.onToken({ id: 'abc123' }, createOrderMock);
+		expect(NProgress.start).toHaveBeenCalled();
+		expect(createOrderMock).toHaveBeenCalled();
+		expect(createOrderMock).toHaveBeenCalledWith({ "variables": { "token": "abc123" } });
+	});
+
+	it('routes to order page when completed', async () => {
+		const wrapper = mount(
+			<MockedProvider mocks={mocks}>
+				<Charge/>
+			</MockedProvider>
+		);
+		await wait();
+		wrapper.update();
+		const createOrderMock = jest.fn().mockResolvedValue({
+			data: {
+				createOrder: {
+					id: 'abc123',
+				},
+			},
+		});
+		const component = wrapper.find('Charge').instance();
+
+		Router.router.push = jest.fn();
+		component.onToken({ id: 'abc123' }, createOrderMock);
+
+		await wait();
+		expect(Router.router.push).toHaveBeenCalled();
+		expect(Router.router.push).toHaveBeenCalledWith({ "pathname": "/order", "query": { "id": "abc123" } });
 	});
 });
